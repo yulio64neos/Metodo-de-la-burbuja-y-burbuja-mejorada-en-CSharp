50324f83492f2bfc56884df74a80c9aabade8874
yulio64neos <julio64neos@gmail.com>
2022-09-20 13:39:56 -0500

    Falta el ordenamiento en XML

---
+++ MetodoDeLaBurbuja/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MetodoDeLaBurbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(62, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "METODOS DE LA BURBUJA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Metodo de la burbuja";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dame un número";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Ordenar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Metodo de la burbuja Mejorado";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(191, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Ordenado Mejorado";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(299, 135);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(191, 264);
+            this.listBox2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(299, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Importar XML";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(548, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(451, 150);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ MetodoDeLaBurbuja/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace MetodoDeLaBurbuja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        //List<int> arrayNum = null;
+
+        
+        int[] lolo = { 0, 2, 1, 5, };
+        private List<int> arrayNum = new List<int>();
+        public void burbuja_PWA_NADAQUEVER()
+        {
+            int numTemp;
+            for (int j = 0; j < arrayNum.Count; j++)
+            {
+                for (int i = 0; i < arrayNum.Count - 1; i++)
+                {
+                    if (arrayNum[i] > arrayNum[i + 1])
+                    {
+                        numTemp = arrayNum[i + 1];
+                        arrayNum[i + 1] = arrayNum[i];
+                        arrayNum[i] = numTemp;
+                    }
+                }
+            }
+
+            if (listBox1.InvokeRequired)
+                listBox1.Invoke(new MethodInvoker(delegate
+                {
+                    foreach (int p in arrayNum)
+                        listBox1.Items.Add(p.ToString());
+                }));
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //arrayNum = Convert.ToInt32(textBox1.Text);
+            
+            listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+            listBox2.Items.Add(Convert.ToInt32(textBox1.Text));
+            arrayNum.Add(Convert.ToInt32(textBox1.Text));
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Thread miHilo = new Thread(delegate ()
+            {
+                burbuja_PWA_NADAQUEVER();
+                if (listBox1.InvokeRequired)
+                {
+                    listBox1.Invoke(new MethodInvoker(delegate
+                    {
+                        listBox1.Items.Add("Terminó el proceso JUAS JUAS");
+                    }));
+                }
+            });
+            miHilo.Start();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public int[] BurbujaMejorado_NADAQUEVER_PWA(int[] n)
+        {
+            bool banderasJUASJUAS;
+            int temp, t, i;
+            t = n.Length;
+            banderasJUASJUAS = false;
+            i = 0;
+            while (!banderasJUASJUAS)
+            {
+                banderasJUASJUAS = true;
+                {
+                    for (int j = t - 1; j > i; j--)
+                    {
+                        if (n[j] < n[j - 1])
+                        {
+                            temp = n[j];
+                            n[j] = n[j - 1];
+                            n[j - 1] = temp;
+                            banderasJUASJUAS = false;
+                        }
+                    }
+                }
+                i++;
+            }
+            return n;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            //int[] ArregloMejorado;
+
+            Thread miHilo = new Thread(delegate ()
+            {
+                var ArregloMejorado = BurbujaMejorado_NADAQUEVER_PWA(arrayNum.ToArray());
+                if (listBox2.InvokeRequired)
+                {
+                    listBox2.Invoke(new MethodInvoker(delegate
+                    {
+                        foreach (int item in ArregloMejorado)
+                        {
+                            listBox2.Items.Add(item);
+                        }
+                        listBox2.Items.Add("Terminó el proceso 2 JUAS JUAS");
+                    }));
+                }
+            });
+            miHilo.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                string xmlFilePath = op.FileName;
+                using (DataSet ds = new DataSet())
+                {
+                    ds.ReadXml(xmlFilePath);
+                    dataGridView1.DataSource = ds.Tables[0];
+                    listBox1.DataSource = ds.Tables[0];
+                    listBox1.DisplayMember = "descripcion";
+                    listBox1.ValueMember = "descripcion";
+                    listBox2.DataSource = ds.Tables[0];
+                    listBox2.DisplayMember = "descripcion";
+                    listBox2.ValueMember = "descripcion";
+                    MessageBox.Show(ds.Tables[0].Rows.Count + " Total de registros cargados.");
+                }
+            }
+        }
+
+        private List<string> arrayString = new List<string>();
+        public void burbuja_PWA_NADAQUEVER_string()
+        {
+            int numTemp;
+            for (int j = 0; j < arrayNum.Count; j++)
+            {
+                for (int i = 0; i < arrayNum.Count - 1; i++)
+                {
+                    if (arrayNum[i] > arrayNum[i + 1])
+                    {
+                        numTemp = arrayNum[i + 1];
+                        arrayNum[i + 1] = arrayNum[i];
+                        arrayNum[i] = numTemp;
+                    }
+                }
+            }
+
+            if (listBox1.InvokeRequired)
+                listBox1.Invoke(new MethodInvoker(delegate
+                {
+                    foreach (int p in arrayNum)
+                        listBox1.Items.Add(p.ToString());
+                }));
+        }
+    }
+}
